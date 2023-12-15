87efb6182983864765d441fae9fc4a3e3e90df6a
RainyCarnival <andy.sherrer@hotmail.ca>
2023-12-15 03:25:58 -0500

    Some navigation working now, but there are problems, like the search button crashing the app when on the second fragment.

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -32,6 +32,12 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        // Assuming searchButton is in the MainActivity layout
+        binding.searchButton.setOnClickListener {
+            // Handle the click event here
+            navController.navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
